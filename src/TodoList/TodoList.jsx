e38fbf14785e900f52e6c38519cabf169ca59dfa
Mahdi <mahdi.unic1@gmail.com>
2023-06-27 10:02:02 +0200

    local storage save and display

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './todoList.scss'
+import { Josefin_Sans } from 'next/font/google'
 
 const data = [
     "Faire les course",
@@ -13,7 +14,8 @@ const data = [
 
 const TodoList = ()=>{
 
-    const [tasks, setTasks] = useState(data)
+    const [tasks, setTasks] = useState()
+    const [loading, setLoading] = useState(true)
     const inputRef = useRef()
 
     const handleTaskValidation = (taskIndex) =>{
@@ -34,31 +36,60 @@ const TodoList = ()=>{
         }
     }
 
+    const handleResetButton = () =>{
+        setTasks(data)
+    }
+
+    // Get and display data from localstorage
+    useEffect(()=>{
+        const localStorageData = localStorage.getItem('Test')
+
+        if(localStorageData){
+            setTasks(JSON.parse(localStorageData))
+        }else{
+            setTasks(data)
+        }
+
+        setLoading(false)
+    }, [])
+
+    // Save data in localStorage
+    useEffect(()=>{
+        localStorage.setItem('Test' , JSON.stringify(tasks))
+    }, [tasks] )
+
+
+
+
     return (
-        <main className="todoList">
-            <div className="todoContainer">
-                <button>Reset</button>
-
-                <ul className='todoListItems'>
-
-                    {tasks.map((task, index) => (
-
-                        
-                        <li className='todoSection' key={index} onClick={()=>{handleTaskValidation(index)}}>
-                            <p>{task}</p>
-                        </li>
-
-                    ))}
-
-                </ul>
-
-                <div className="todoListFunctions">
-                    <input ref={inputRef} type="text" />
-                    <button onClick={handleNewTask} >Ajouter</button>
+        <>
+            {!loading && (
+                <main className="todoList">
+                <div className="todoContainer">
+                    <button onClick={handleResetButton}>Reset</button>
+    
+                    <ul className='todoListItems'>
+    
+                        {tasks.map((task, index) => (
+    
+                            
+                            <li className='todoSection' key={index} onClick={()=>{handleTaskValidation(index)}}>
+                                <p>{task}</p>
+                            </li>
+    
+                        ))}
+    
+                    </ul>
+    
+                    <div className="todoListFunctions">
+                        <input ref={inputRef} type="text" />
+                        <button onClick={handleNewTask} >Ajouter</button>
+                    </div>
                 </div>
-            </div>
-            
-        </main>
+                
+            </main>
+            )}
+        </>
     )
 }
 
